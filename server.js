c99658df5844cbdd9f58d53f32349337608f29e0
chetna <chetnagarg019@gmail.com>
2026-04-05 00:02:50 +0530

    commit #2

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ import express from "express";
 import blog from "./routes/blog.js";
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use("/blog", blog);
 
@@ -61,3 +61,4 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
+//practice first backend proj
